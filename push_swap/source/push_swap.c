942c6c64d3129d3b2f7709e6efabcc005894d70b
aydiler <ahmet.diler@hotmail.de>
2024-08-21 23:29:09 +0200

    implemented radix sort

--- push_swap/source/push_swap.c
+++ push_swap/source/push_swap.c
@@ -1,31 +1,67 @@
 #include "../includes/push_swap.h"
 
+static int	get_max_bits(t_stack *stack)
+{
+	t_list	*current;
+	long	max;
+	int		max_bits;
+
+	current = stack->head;
+	max = 0;
+	while (current)
+	{
+		if ((long)current->content > max)
+			max = (long)current->content;
+		current = current->next;
+	}
+	max_bits = 0;
+	while ((max >> max_bits) != 0)
+		max_bits++;
+	return (max_bits);
+}
+
+static void	radix_sort_bit(t_stack *a, t_stack *b, int bit)
+{
+	int		size;
+	int		i;
+	long	num;
+
+	size = ft_lstsize(a->head);
+	i = 0;
+	while (i < size)
+	{
+		num = (long)a->head->content;
+		if (((num >> bit) & 1) == 1)
+			ft_ra();
+		else
+			ft_pb();
+		i++;
+	}
+	while (b->head)
+		ft_pa();
+}
+
+void	radix_sort(t_stack *a, t_stack *b)
+{
+	int	max_bits;
+	int	i;
+
+	max_bits = get_max_bits(a);
+	i = 0;
+	while (i < max_bits)
+	{
+		radix_sort_bit(a, b, i);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc < 2)
 		return (1);
 	if (!process_arguments(argc, argv))
 		return (1);
-	// print_stacks(stack_a(), stack_b());
-	ft_pb();
-	ft_pb();
-	ft_pb();
-	ft_pb();
-	ft_pb();
-	print_stacks(stack_a(), stack_b());
-	// ft_ra();
-	// ft_rb();
-	// print_stacks(stack_a(), stack_b());
-	// ft_rr();
-	// print_stacks(stack_a(), stack_b());
-	// ft_rra();
-	// print_stacks(stack_a(), stack_b());
-	// ft_rrb();
-	// print_stacks(stack_a(), stack_b());
-	// ft_rrr();
-	// print_stacks(stack_a(), stack_b());
-	ft_sa();
-	ft_sb();
+	radix_sort(stack_a(), stack_b());
 	print_stacks(stack_a(), stack_b());
 	free_stack(stack_a());
 	free_stack(stack_b());
